57555ae325aa5ce07be50483ea1a502b93f8b768
venisha216 <venisha2185@gmail.com>
2025-12-31 14:42:48 +0530

    apply iniital django migrations

--- backend/smart_library/settings.py
+++ backend/smart_library/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'books',
+    'reservations',
+    'fines',
+    'chatbot',
+
 ]
 
 MIDDLEWARE = [
